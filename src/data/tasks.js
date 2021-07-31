677462b5d8b26c0c503d751c07b1762ebc2d07e0
sarah-maau <maurysarah@hotmail.fr>
2021-07-31 10:20:30 +0200

    add data (tasks)

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    label: 'Procrastiner',
+    done: true,
+  },
+  {
+    id: 13,
+    label: 'Paniquer',
+    done: false,
+  },
+  {
+    id: 4,
+    label: 'Apprendre React',
+    done: false,
+  },
+  {
+    id: 8,
+    label: 'Finir la saison 1 de Loki',
+    done: false,
+  },
+];
